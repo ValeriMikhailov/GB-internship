64994beb1c04caf70c5d436293a57b33457893e8
Alexey Shein <strategdzr@gmail.com>
2017-03-03 11:22:40 +0300

    add javafx form

--- AdminGUI/src/Main.java
+++ AdminGUI/src/Main.java
@@ -1,5 +1,23 @@
-/**
- * Created by Alexey Shein on 03/03/2017.
- */
-public class Main {
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
