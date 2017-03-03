24d53bc7723ce19f642b46138c0899dfc3873e6e
Alexey Shein <strategdzr@gmail.com>
2017-03-03 22:13:48 +0300

    change Main

--- AdminGUI/src/Main.java
+++ AdminGUI/src/Main.java
@@ -8,8 +8,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("admingui.fxml"));
+        primaryStage.setTitle("AdminGUI");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
