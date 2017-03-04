00fc9f60017fdd57b05a35424c1387c8b4e5ec2b
Alexey Shein <strategdzr@gmail.com>
2017-03-04 17:34:08 +0300

    add updated design

--- AdminGUI/src/Main.java
+++ AdminGUI/src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("admingui.fxml"));
         primaryStage.setTitle("AdminGUI");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
 
