12de333a75eb5c6c9bca9c71c1eec630e894a8d8
AlexandrSedov <alexandrsed@gmail.com>
2017-03-10 20:20:51 +0300

    add class for insert into table "sites"

---
+++ src/DB/SitesHandler.java
@@ -0,0 +1,18 @@
+package DB;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SitesHandler extends  DBHandler {
+    private static PreparedStatement ps;
+
+    public void insert(String siteName) {
+        try {
+            ps = conn.prepareStatement("INSERT INTO sites (id, Name) VALUES (null, ?)");
+            ps.setString(1, siteName);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
