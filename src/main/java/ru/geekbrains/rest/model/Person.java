79f86ffe13893426bf6e473b56de74a4627ac3cf
Fishbonne <simonkorobov@gmail.com>
2017-03-10 13:38:22 +0300

    Adding entities

---
+++ src/main/java/ru/geekbrains/rest/model/Person.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.rest.model;
+
+public class Person {
+
+    private Integer id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/geekbrains/rest/model/Site.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.rest.model;
+
+public class Site {
+
+    private Integer id;
+    private String name;
+
+    public Site() {
+    }
+
+    public Site(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
