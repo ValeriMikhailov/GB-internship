ae9393d92e0b5ba9445ac610e309640bed8061b9
Fishbonne <simonkorobov@gmail.com>
2017-03-10 14:10:20 +0300

    Adding methods to UserController

--- src/main/java/ru/geekbrains/rest/web/UserController.java
+++ src/main/java/ru/geekbrains/rest/web/UserController.java
@@ -1,7 +1,37 @@
 package ru.geekbrains.rest.web;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.rest.model.Person;
+import ru.geekbrains.rest.model.Site;
+import ru.geekbrains.rest.service.UserService;
+
+import java.util.Set;
 
 @RestController
+@RequestMapping(UserController.USER_REST_URL)
 public class UserController {
+    static final String USER_REST_URL = "/user";
+
+    @Autowired
+    private UserService service;
+
+    public Set<Person> getPersons() {
+        return null;
+    }
+
+    public Set<Site> getSites() {
+        return null;
+    }
+
+    public ResponseEntity getStats() {
+        return null;
+    }
+
+    public ResponseEntity getStatsByDate() {
+        return null;
+    }
 }
