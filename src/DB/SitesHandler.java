df10a4ca61b847d85fcc19ab5b7006e3b7fbe54a
AlexandrSedov <alexandrsed@gmail.com>
2017-03-12 22:54:44 +0300

    connect to DB in constructor

--- src/DB/SitesHandler.java
+++ src/DB/SitesHandler.java
@@ -6,6 +6,11 @@ import java.sql.SQLException;
 public class SitesHandler extends  DBHandler {
     private static PreparedStatement ps;
 
+    public SitesHandler() {
+        super();
+        connect();
+    }
+
     public void insert(String siteName) {
         try {
             ps = conn.prepareStatement("INSERT INTO sites (id, Name) VALUES (null, ?)");
