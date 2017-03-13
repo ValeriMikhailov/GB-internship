a84e1ec9c34e5075339653a4ec3d21af736dd0ac
Anatoly <antauren@yandex.ru>
2017-03-13 23:48:09 +0300

    добовлены параметре к функции вывода нак темплейт

--- daily_stat/views.py
+++ daily_stat/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from fake_bd import *
 
 # Create your views here.
 
 def show_daily_page(request):
-    return render(request, 'everyday.html')
+    title = 'Ежедневная статистика'
+    return render(request, 'everyday.html',{'persons': persons,
+                                            'title': title
+                                            })
--- general_stat/views.py
+++ general_stat/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from import_bd import *
 
 # Create your views here.
 
 
 def show_general_page(request):
-    return render(request, 'common.html')
+    title = 'Общая статистика'
+    return render(request, 'common.html',{'sites': sites,
+                                          'list_sites': list_sites,
+                                          'title': title
+                                          })
