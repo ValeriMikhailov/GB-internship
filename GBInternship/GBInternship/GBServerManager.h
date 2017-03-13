ef8b9da30aed1152e6262e07601f4b0d895bad2e
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-03-13 20:17:59 +0200

    Added GBServerManager class

--- GBInternship/GBInternship/GBServerManager.h
+++ GBInternship/GBInternship/GBServerManager.h
@@ -1,15 +1,15 @@
 //
-//  ViewController.h
+//  GBServerManager.h
 //  GBInternship
 //
 //  Created by Stanly Shiyanovskiy on 13.03.17.
 //  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+@interface GBServerManager : NSObject
 
++ (GBServerManager*) sharedManager;
 
 @end
-
---
+++ GBInternship/GBInternship/GBServerManager.m
@@ -0,0 +1,13 @@
+//
+//  GBServerManager.m
+//  GBInternship
+//
+//  Created by Stanly Shiyanovskiy on 13.03.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import "GBServerManager.h"
+
+@implementation GBServerManager
+
+@end
