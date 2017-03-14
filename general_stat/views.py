7354aa83e5158a43c98fcf31c9036958a714afcb
vladisgrig <vladisgrig@yandex.ru>
2017-03-15 00:20:52 +0300

    Добавлено взаимодействие с веб-сервисом для общей статистики Отредактирован файл настроек для доступы к REST-методам

--- general_stat/views.py
+++ general_stat/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
-from import_bd import *
+import import_bd
+import requests
 
 # Create your views here.
 
+def get_sites():
+    request = requests.get(import_bd.sites_url)
+    sites = request.json()
+    return sites
+
+def get_general_stat(site_id):
+    request = requests.get(import_bd.general_stat_url + str(site_id))
+    general_stat = [x for x in request.json() if x["siteId"] == site_id]
+    print(general_stat)
+    return general_stat
 
 def show_general_page(request):
     title = 'Общая статистика'
+
+    sites = get_sites()
+
+    if request.method == "POST":
+        site_id = int(request.POST["site_id"][0])
+        general_stat = get_general_stat(site_id)
+    else:
+        site_id = sites[0]["id"]
+        general_stat = get_general_stat(site_id)
+
     return render(request, 'common.html',{'sites': sites,
-                                          'list_sites': list_sites,
-                                          'title': title
+                                          'general_stat': general_stat,
+                                          'title': title,
+                                          'site_id': site_id
                                           })
