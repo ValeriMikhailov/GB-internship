a24083de73e9a39e3c0835eb2884b6b5a7e0d340
Alexey Shein <strategdzr@gmail.com>
2017-03-15 10:21:02 +0300

    add libs add jsonparcer and restapi plug

---
+++ AdminGUI/src/JsonElement.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Alexey Shein on 15/03/2017.
+ * Describing of JSON fields
+ */
+public class JsonElement {
+
+    private String id;
+    private String name;
+    private String date;
+    private String personName;
+    private String count;
+    private String countNewPages;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public String getCountNewPages() {
+        return countNewPages;
+    }
+}
---
+++ AdminGUI/src/JsonObject.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Created by Alexey Shein on 15/03/2017.
+ * Class for describing data object from JDON
+ */
+public class JsonObject {
+
+    private List<JsonElement> jsonElements;
+
+    public List<JsonElement> getJsonElements() {
+        return jsonElements;
+    }
+}
