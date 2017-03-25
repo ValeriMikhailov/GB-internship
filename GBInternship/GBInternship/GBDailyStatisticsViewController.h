d7bdca3a68cb7c245cbebd38092dd8b0c82fffe6
Irina Dorofeeva <idorofeeva@gmail.com>
2017-03-24 19:17:32 -0700

    Worked on Daily Statistics View

---
+++ GBInternship/GBInternship/GBDailyStatisticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBDailyStatisticsViewController.h
+//  GBInternship
+//
+//  Created by Mac on 3/24/17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBDailyStatisticsViewController : UIViewController
+
+@end
