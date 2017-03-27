1bac1e7c767dca6559121b7d20c292e0d14f8327
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2017-03-27 17:08:29 +0700

    полностью готова таба Global. Всё работает по http.

--- StatisticsApp/app/src/main/java/ru/geekbrains_internship/statisticsapp/Global/GlobalFragment.java
+++ StatisticsApp/app/src/main/java/ru/geekbrains_internship/statisticsapp/Global/GlobalFragment.java
@@ -1,16 +1,29 @@
 package ru.geekbrains_internship.statisticsapp.Global;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import ru.geekbrains_internship.statisticsapp.R;
 
@@ -19,6 +32,24 @@ import ru.geekbrains_internship.statisticsapp.R;
  */
 
 public class GlobalFragment extends Fragment {
+
+    private static final String URL_BASIC = "http://52.89.213.205:9000/rest/user/";
+
+    private final String TAG = "GlobalFragment";
+
+    private ProgressBar progressBar;
+
+    ArrayList<HashMap<String, String>> personList;
+    ListView listView;
+    ScrollView sc;
+    MaterialSpinner spinner;
+    List<String> sitesForSpinner;
+    HashMap<String, Integer> siteItem;
+    String name;
+    String url;
+    TextView jopa;
+    int idSite;
+    SimpleAdapter adapter;
 
     public GlobalFragment() {
     }
@@ -32,22 +63,239 @@ public class GlobalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_global, container, false);
 
-        ListView listView = (ListView) view.findViewById(R.id.listViewGlobalFragment);
-        final String[] names = new String[] {"Путин", "Навальный", "Жириновский", "Медведев"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, names);
-        listView.setAdapter(adapter);
+        sc = (ScrollView) view.findViewById(R.id.scrollViewGlobal);
+        jopa = (TextView) view.findViewById(R.id.jopa);
+        jopa.setVisibility(View.INVISIBLE);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
-        TextView lastUpdate = (TextView) view.findViewById(R.id.lastUpdateTextView);
-        lastUpdate.setText(getResources().getString(R.string.last_update) + "  Это надо сделать!!!");
+        personList = new ArrayList<>();
+        sitesForSpinner = new ArrayList<>();
+        siteItem = new HashMap<>();
 
-        MaterialSpinner spinner = (MaterialSpinner) view.findViewById(R.id.spinnerSiteGlobalFragment);
-        spinner.setItems("lenta.ru", "pochta.ru", "android.com", "material.io", "mail.ru");
+        listView = (ListView) view.findViewById(R.id.listViewGlobalFragment);
+
+        spinner = (MaterialSpinner) view.findViewById(R.id.spinnerSiteGlobalFragment);
+
+        //получаем список сайтов для спиннера
+        new getSites().execute();
+
+        //узнаем url для загрузки в листвью на основе выбранного пункта в спиннере
+        WhatsUrl();
+
+        //получаем список персон по url
+        new GetPersonsGlobal().execute();
+
         spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
             @Override
             public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
                 Toast.makeText(getActivity(), "Выбран " + item, Toast.LENGTH_SHORT).show();
+                idSite = siteItem.get(item);
+                Log.e(TAG, "idSite = " + String.valueOf(idSite));
+                WhatsUrl();
+                jopa.setVisibility(View.INVISIBLE);
+                new GetPersonsGlobal().execute();
+              //  synchronized (adapter) {
+
+//                    adapter.notifyDataSetChanged();
+              //  }
+
             }
         });
+
+        //listView = (ListView) view.findViewById(R.id.listViewGlobalFragment);
+        //new GetPersonsGlobal().execute();
+
+        TextView lastUpdate = (TextView) view.findViewById(R.id.lastUpdateTextView);
+        lastUpdate.setText(getResources().getString(R.string.last_update) + "  Это надо сделать!!!");
+
+
         return view;
+    }
+
+    /**
+     * Asynctask для получения json по HTTP-запросу (список имен и количество упоминаний)
+     */
+    private class GetPersonsGlobal extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // показываем progressbar
+            progressBar.setActivated(true);
+            progressBar.setVisibility(View.VISIBLE);
+            personList.clear();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+//            //определяем id выбранного в спиннере сайта
+//            int idSite = siteItem.get(name);
+//            //формируем url по этому id
+//            url = URL_BASIC + idSite;
+//            Log.e(TAG, url);
+
+            // запрос по url и получение ответа
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    Thread.sleep(1000); //искусственная задержка для показать progressbar
+
+                    JSONArray person = new JSONArray(jsonStr);
+
+                    // проходимся по всем пунктам
+                    for (int i = 0; i < person.length(); i++) {
+                        JSONObject c = person.getJSONObject(i);
+
+                        String personName = c.getString("personName");
+                        String rank = c.getString("rank");
+
+                        // hash map для каждого пункта листвью
+                        HashMap<String, String> personItem = new HashMap<>();
+
+                        // добавление каждого элемента в HashMap key => value
+                        personItem.put("rank", rank);
+                        personItem.put("personName", personName);
+
+                        // добавление в лист
+                        personList.add(personItem);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // убираем индикатор загрузки
+            if (progressBar.isActivated())
+                progressBar.setVisibility(View.INVISIBLE);
+
+            if (personList.isEmpty()) {
+                listView.setVisibility(View.INVISIBLE);
+                jopa.setVisibility(View.VISIBLE);
+
+            } else {
+                //обновляем данные в ListView
+                adapter = new SimpleAdapter(getActivity(), personList,
+                        R.layout.listview_item_global, new String[]{"personName", "rank"},
+                        new int[]{R.id.personName, R.id.rank});
+                listView.setVisibility(View.VISIBLE);
+                listView.setAdapter(adapter);
+            }
+        }
+
+    }
+
+    /**
+     * Asynctask для получения json по HTTP-запросу (список сайтов)
+     */
+    private class getSites extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // запрос по url и получение ответа
+            String jsonStr = sh.makeServiceCall(URL_BASIC + "sites");
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONArray sites = new JSONArray(jsonStr);
+
+                    // проходимся по всем пунктам
+                    for (int i = 0; i < sites.length(); i++) {
+                        JSONObject c = sites.getJSONObject(i);
+
+                        int id = c.getInt("id");
+                        name = c.getString("name");
+
+                        // добавление каждого элемента в HashMap key => value
+                        siteItem.put(name, id);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error (spinner): " + e.getMessage());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(),
+                                    "Json parsing error (spinner): " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            //обновляем данные в спиннере
+            sitesForSpinner = new ArrayList<>(siteItem.keySet());
+            spinner.setItems(sitesForSpinner);
+        }
+    }
+
+
+    //метод определения нужного url для листвью в зависимости от выбранного пункта в спиннере
+    private void WhatsUrl() {
+        //определяем id выбранного в спиннере сайта
+//        idSite = siteItem.get(name);
+        //формируем url по этому id
+        url = URL_BASIC + idSite;
+        Log.e(TAG, url);
+        //return url;
     }
 }
---
+++ StatisticsApp/app/src/main/java/ru/geekbrains_internship/statisticsapp/Global/HttpHandler.java
@@ -0,0 +1,70 @@
+package ru.geekbrains_internship.statisticsapp.Global;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by GooDi on 23.03.2017.
+ */
+
+public class HttpHandler {
+
+    private static final String TAG = "HttpHandler";
+
+    public HttpHandler() {
+    }
+
+    //метод обращения по url и получение ответа в виде строки
+    public String makeServiceCall(String reqUrl) {
+        String response = null;
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            // читаем ответ от сервера
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+        return response;
+    }
+
+    //инпут стрим конвертим в строку
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+}
